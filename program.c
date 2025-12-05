a19e22c609cbc4010a88547a51b4b0d14496065e
Ognjen696 <oggirankovic@gmail.com>
2025-12-05 09:28:10 +0100

    Update program.c
    nesto se promenilo

--- program.c
+++ program.c
@@ -1,5 +1,13 @@
 #include<stdio.h>
 int main(){
-  printf("Dobrodosli na kurs Linuxa");
+  printf("rDobodosli na kurs Linuxa");
+  printf("rDobodosli na kurs Linuxa");
+  printf("rDobodosli na kurs Linuxa");
+  printf("rDobodosli na kurs Linuxa");
+  printf("rDobodosli na kurs Linuxa");
+  printf("rDobodosli na kurs Linuxa");
+  printf("rDobodosli na kurs Linuxa");
+  printf("rDobodosli na kurs Linuxa");
+  printf("rDobodosli na kurs Linuxa");
 return 0;
 }
